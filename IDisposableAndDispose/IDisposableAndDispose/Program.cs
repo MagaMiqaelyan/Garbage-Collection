e0903b6970e421ac5471ddd71607f71e0fb199c2
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-26 11:57:16 +0400

    dispose

---
+++ IDisposableAndDispose/IDisposableAndDispose/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace IDisposableAndDispose
+{
+    public class Base : IDisposable
+    {
+        public Base()
+        {
+            Func();
+        }
+        public virtual void Dispose()
+        {
+            Func();
+        }
+        public virtual void Func()
+        {
+            Console.WriteLine("Base class");
+        }
+
+    }
+
+    public class Child : Base
+    {
+        public Child()
+        {
+            Func();
+        }
+
+        public override void Dispose()
+        {
+            Func();
+            base.Dispose();
+        }
+
+        public override void Func()
+        {
+            Console.WriteLine("Child class");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (Base obj = new Child())
+            {
+                obj.Func();
+            }           
+        }
+    }
+
+}
