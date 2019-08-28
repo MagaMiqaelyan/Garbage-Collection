149fd76f14abb013505bbdb7c55342d1e381028d
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-28 23:51:50 +0400

    finalize implemented

---
+++ Finalize/Finalize/SQLConnector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Finalize
+{
+    public class SQLConnector : IDisposable
+    {
+        string connectionString = "Data Source=(local);" +
+                     "Initial Catalog=AdventureWorks_Final;" +
+                     "Integrated Security=true";
+        private SqlConnection sqlConn = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SQLConnector()
+        {
+            sqlConn = new SqlConnection(connectionString);
+            sqlConn.Open();
+        }
+
+        public DataSet GetData()
+        {
+            //Add the code to retireve some data from the DataBase
+            return new DataSet();
+        }
+
+        public void PutData()
+        {
+            //Add the code to update the data base
+        }
+
+        ~SQLConnector()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+            }
+            ReleaseNativeResource();
+        }
+
+        private void ReleaseNativeResource()
+        {
+            sqlConn.Dispose();
+        }
+    }
+}
